a5b2eb8076fd68b12793ceb1068bbbb0387c5333
maps4all <maps4all007@yandex.com>
2020-06-26 09:42:34 +0200

    Create plugin.js
    Pulgin added.

---
+++ plugin.js
@@ -0,0 +1,6 @@
+{
+    "moduleId" : "grabowCommuter/germany_BW",    
+    "type" : "RSS",    
+    "name" : "Polizei Baden-Württemberg",    
+    "uri" : "https://verkehrsmeldungen.polizei-bw.de/TicRss.ashx?region=BW"
+}
